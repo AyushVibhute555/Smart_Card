44620243b42d83c3caa1c16384caa59ef483b8f1
Ayush Vibhute <123165334+AyushVibhute555@users.noreply.github.com>
2025-11-30 10:00:32 +0530

    Delete SmartCart directory

--- SmartCart/backend-java/src/main/java/com/smartcart/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcart.repository;
-
-import com.smartcart.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByNameIgnoreCase(String name);
-}
